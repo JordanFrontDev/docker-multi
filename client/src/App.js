84bcbb1a20624a41df0fa20f974d7ea3763eda34
Jordan Vinicius <jordan.hue@hotmail.com>
2020-01-20 13:45:07 -0300

    change text

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React 
+            Learn React Fib
         </a>
         </header>
       </div>
